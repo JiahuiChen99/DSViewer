10f05212d858c78dac5c1300dbdb59047c617fa2
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-01 16:27:56 +0000

    chore: app set up boilerplate

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <!-- Page rendered by router -->
+        <router-view/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    components: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
