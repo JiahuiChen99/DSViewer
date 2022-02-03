cc0e21d307955f5a302565874de1cb4fc5a7c08e
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-03 13:19:31 +0000

    #3 feat: import data section methods

--- src/pages/ImportData/ImportDataPage.vue
+++ src/pages/ImportData/ImportDataPage.vue
@@ -30,8 +30,80 @@
 </template>
 
 <script>
+import UploadConfirmation from "@/components/ImportData/UploadConfirmation";
+import SvgIcon from "@jamescoyle/vue-icon";
+import {mdiFileUpload} from "@mdi/js";
+import IconButton from "@/components/UI/IconButton";
+import { ref } from 'vue'
+
 export default {
-    name: "ImportDataPage"
+    name: "ImportDataPage",
+    components: {
+        IconButton,
+        SvgIcon,
+        UploadConfirmation
+    },
+    setup() {
+        const active = ref(false)
+        const toggleActive = () => {
+            active.value = !active.value;
+        }
+
+        return { active, toggleActive }
+    },
+    methods: {
+        toggle_popup: function () {
+            this.file_selected = !this.file_selected;
+            this.selected_file_name = this.selected_file.name;
+            this.selected_file = "";
+        },
+        /**
+         * Upload confirmation
+         * Cancel: buttonID 0
+         * Upload: buttonID 1
+         * @param buttonID Identifies pressed button
+         */
+        confirm_upload: function (buttonID) {
+            if ( buttonID ) {
+                // TODO: Process data
+            }
+            // Toggles the popup
+            this.toggle_popup();
+        },
+        /**
+         * Checks whether is a drop or a input change event
+         * Stores the selected file and enables a popup
+         * for the user to confirm the upload to the backend
+         * @param e: drop or change event
+         */
+        select_file: function (e) {
+            let uploaded_file;
+            if (e.type === "drop") {
+                this.toggleActive();
+                uploaded_file = e.dataTransfer.files[0];
+                if ( !uploaded_file ) return;
+                this.data_file = uploaded_file;
+                this.selected_file_name = this.data_file.name;
+                this.confirm_upload();
+                return;
+            }
+            uploaded_file = e.target.files[0];
+            if ( !uploaded_file ) return;
+            this.data_file = uploaded_file;
+            this.selected_file_name = this.data_file.name;
+            this.$refs.ds_file.value = null;
+            this.confirm_upload();
+        },
+    },
+    data() {
+        return {
+            file_selected: false,
+            selected_file_name: "",
+            selected_file: null,
+            show_popUp : false,
+            upload_file_icon: mdiFileUpload
+        }
+    }
 }
 </script>
 
