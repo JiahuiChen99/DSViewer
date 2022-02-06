5d64ae1d55eaec04764853156d620cd5b0143d09
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-06 13:06:30 +0100

    #7 feat: echarts rendering in graph page

--- src/pages/Graph/GraphPage.vue
+++ src/pages/Graph/GraphPage.vue
@@ -1,12 +1,40 @@
 <template>
-    <section id="graph">
-        Graph
+    <section class="flex w-full h-full " id="graph">
     </section>
 </template>
 
 <script>
+import * as echarts from "echarts";
+import graphConfig from "@/assets/echartsConfig/graphConfig";
+
 export default {
-    name: "GraphPage"
+    name: "GraphPage",
+    mounted() {
+        let graphID = document.getElementById('graph');
+        let graph = echarts.init(graphID);
+
+        this.load_series_data();
+        this.load_series_links();
+
+        this.graph_data.series[0].data = this.graph_series_data;
+        this.graph_data.series[0].links = this.graph_series_links;
+        graph.setOption(this.graph_data);
+    },
+    methods: {
+        load_series_data: function () {
+            this.graph_series_data = this.$store.state.echarts_graph_series_data;
+        },
+        load_series_links: function () {
+            this.graph_series_links = this.$store.state.echarts_graph_series_links;
+        }
+    },
+    data() {
+        return {
+            graph_data: graphConfig,
+            graph_series_data: null,
+            graph_series_links: null,
+        }
+    }
 }
 </script>
 
