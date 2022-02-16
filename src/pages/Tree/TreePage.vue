b6e6d9bf603f40f7e2b7ac8c699f69e7b8832804
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-16 08:24:50 +0100

    #11 feat: create tree on component load

--- src/pages/Tree/TreePage.vue
+++ src/pages/Tree/TreePage.vue
@@ -1,12 +1,35 @@
 <template>
-    <section id="tree">
-
+    <section class="flex w-full h-full" id="tree">
     </section>
 </template>
 
 <script>
+import treeConfig from "@/assets/echartsConfig/treeConfig";
+import * as echarts from "echarts";
+
 export default {
-    name: "TreePage"
+    name: "TreePage",
+    mounted() {
+        let treeID = document.getElementById('tree');
+        let tree = echarts.init(treeID, null);
+
+        this.load_series_data();
+
+        this.tree_data.series[0].data = this.tree_series_data;
+
+        tree.setOption(this.tree_data);
+    },
+    methods: {
+        load_series_data: function () {
+            this.tree_series_data = null
+        }
+    },
+    data() {
+        return {
+            tree_data: treeConfig,
+            tree_series_data: null,
+        }
+    }
 }
 </script>
 
